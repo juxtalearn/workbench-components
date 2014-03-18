53e81d29b3ac836e36a6ebec1ecb5c06a491da78
nmalzahn <nm@rias-institute.eu>
2014-03-18 10:21:30 +0100

    simplified startUP

--- components-activitiystreamtograph/src/main/java/org/juxtalearn/rias/components/activitystreamtograph/Activitystreamtograph.java
+++ components-activitiystreamtograph/src/main/java/org/juxtalearn/rias/components/activitystreamtograph/Activitystreamtograph.java
@@ -1,43 +1,16 @@
 package org.juxtalearn.rias.components.activitystreamtograph;
 
 import info.collide.sqlspaces.commons.Tuple;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 import eu.sisob.components.framework.AgentManager;
 
 public class Activitystreamtograph {
 
     public static void main(String args[]) {
-        String serverdata[] = loadServerData();
-        String serverlocation = serverdata[0];
-        int port = Integer.parseInt(serverdata[1]);
-        executeFileLoader(serverlocation, port);
-    }
-
-    public static void executeFileLoader(String serverlocation, int port) {
-        AgentManager flm = new ActivitystreamToGraphManager(new Tuple(String.class, Integer.class, Integer.class, String.class, "Activitystream To Graph Converter", String.class, String.class), "Activitystream To Graph Converter Manager", serverlocation, port);
-        flm.initialize();
-        Thread runtime = new Thread(flm);
+    	AgentManager proManager = new ActivitystreamToGraphManager(new Tuple(String.class, Integer.class, Integer.class, String.class, "Visualization To Server", String.class, String.class), ActivitystreamToGraphManager.class.getName(), "192.168.1.21", 32525);
+        proManager.initialize();
+        Thread runtime = new Thread(proManager);
         runtime.start();
-
+    	
     }
 
-    public static String[] loadServerData() {
-        String serverdata[] = new String[2];
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("server.conf"));
-            // server location
-            serverdata[0] = reader.readLine();
-            // port number
-            serverdata[1] = reader.readLine();
-
-            reader.close();
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return serverdata;
-    }
 }
