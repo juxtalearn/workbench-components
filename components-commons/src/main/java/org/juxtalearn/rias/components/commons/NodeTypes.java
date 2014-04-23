33752903495d80d5ba3fc3b4a25e477304e38a79
Nils Malzahn <malzahn@collide.info>
2014-04-23 11:54:59 +0200

    created new commons module to ease sharing of common definitions

---
+++ components-commons/src/main/java/org/juxtalearn/rias/components/commons/NodeTypes.java
@@ -0,0 +1,50 @@
+package org.juxtalearn.rias.components.commons;
+
+
+public enum NodeTypes {
+    ACTOR("actor", 0),
+    FILE("file", 1),
+    BLOG("blog", 2),
+    COMMENT("generic_comment", 3),
+    FIVESTAR("fivestar", 4),
+    NONE("none", 10),
+    MEMBER("member_of_site", 15),
+    READYET("readYet", 20),
+    SIMPLETYPE("simpletype", 25);
+
+    private String typeString;
+
+    private int typeNumber;
+
+    private NodeTypes(String typeString, int typenumber) {
+        this.typeString = typeString;
+        this.typeNumber = typenumber;
+    }
+
+    public String getTypeString() {
+        return typeString;
+    }
+
+    public int getTypeNumber() {
+        return typeNumber;
+    }
+
+    public static NodeTypes getEnum(int typeNumber) {
+        NodeTypes[] arr = NodeTypes.values();
+        for (int i = 0; i < arr.length; i++) {
+            if (typeNumber == arr[i].getTypeNumber())
+                return arr[i];
+        }
+        return null;
+    }
+
+    public static NodeTypes getEnum(String typeString) {
+        NodeTypes[] arr = NodeTypes.values();
+        for (int i = 0; i < arr.length; i++) {
+            if (typeString.equals(arr[i].getTypeString()))
+                return arr[i];
+        }
+        System.err.println("Could not find: " + typeString);
+        return null;
+    }
+}
